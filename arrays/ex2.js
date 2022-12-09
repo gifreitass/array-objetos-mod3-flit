4757917915f6cec0cabd693e21ec4e3ce89ddf79
Giovanna de Freitas <giovannadefreitasaraujo@hotmail.com>
2022-12-08 23:24:42 -0300

    adicionando correções para os exercícios

--- arrays/ex2.js
+++ arrays/ex2.js
@@ -1,24 +1,16 @@
 function PontodeOnibus(paradas){
-    let passageirosEntram = []
-    let passageirosSaem = []
+    let passageirosEntram = 0
+    let passageirosSaem = 0
     let passageirosFinal = 0
 
     paradas.forEach(passageiros => {
-        passageirosEntram.push(passageiros[0])
-        passageirosSaem.push(passageiros[1])
+    passageirosEntram += passageiros[0]
+    passageirosSaem += passageiros[1]
     });
 
-    let somaPassageirosEntram = 0
-    for (let i = 0; i < passageirosEntram.length; i++) {
-        somaPassageirosEntram += passageirosEntram[i]
-    }
+    passageirosFinal = passageirosEntram - passageirosSaem
 
-    let somaPassageirosSaem = 0
-    for (let i = 0; i < passageirosSaem.length; i++) {
-        somaPassageirosSaem += passageirosSaem[i]
-    }
-
-    passageirosFinal = somaPassageirosEntram - somaPassageirosSaem
+    console.log(passageirosFinal)
 
     return passageirosFinal
 }
--- arrays/ex3.js
+++ arrays/ex3.js
@@ -3,13 +3,11 @@ function bingo(numeros){
     
     for (let i = 0; i < numerosBingo.length; i++) {
         let numerosCorretos = numerosBingo[i];
-        if(numeros.includes(numerosCorretos)){
-            return 'GANHOU'
-        } else {
+        if(!numeros.includes(numerosCorretos)){
             return 'PERDEU'
-        }        
+        }  
     }
-
+    return 'GANHOU'  
 }
 
 bingo([21, 13, 2, 7, 5, 14, 7, 15, 9, 10]) 
